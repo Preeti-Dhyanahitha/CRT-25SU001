886c021b452c166651aec048392e4333dba2b9d5
preeti <preeti@dhyanahitha.com>
2026-01-28 16:48:53 +0530

    Day 28-1-2026

--- IV th-sem/Python/Batch-1B/practice/M01_Language_and_Programming_Basics/S04/PS07_Array_basic.py
+++ IV th-sem/Python/Batch-1B/practice/M01_Language_and_Programming_Basics/S04/PS07_Array_basic.py
@@ -1,3 +1,4 @@
+import numpy as np
 '''
 import array
 arr = array.array('i',[])
--- IV th-sem/Python/Batch-1C/practice/M01_Language_and_Programming_Basics/S04/PS07_Debugging.py
+++ IV th-sem/Python/Batch-1C/practice/M01_Language_and_Programming_Basics/S04/PS07_Debugging.py
@@ -1,4 +1,8 @@
 import random
-li = [1,2,3]
-i = random.randint(0,3)
-print(i,li[i])
+import pdb
+arr = [1,2,3,4,5]
+i = random.randint(1,5)
+pdb.set_trace()
+print(arr[i])
+
+
