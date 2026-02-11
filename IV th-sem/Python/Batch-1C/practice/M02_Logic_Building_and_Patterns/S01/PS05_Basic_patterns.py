32e561778257c0e6103acf2d72ec3ee49e8cf2dc
preeti <preeti@dhyanahitha.com>
2026-02-11 16:50:24 +0530

    11-2-2026

---
+++ IV th-sem/Python/Batch-1C/practice/M02_Logic_Building_and_Patterns/S01/PS05_Basic_patterns.py
@@ -0,0 +1,61 @@
+'''
+Basic Patterns:
+1. Square Star Pattern
+n = 4
+Output:
+* * * *
+* * * *
+* * * *
+* * * *
+
+2. Right Angle Triangle
+n = 4
+Output:
+*
+* *
+* * *
+* * * *
+3. Inverted Right Angle Triangle
+n = 4   
+* * * *
+* * *
+* *
+*
+4. Number Triangle
+n = 4
+Output:
+1
+1 2
+1 2 3
+1 2 3 4
+5. Repeated Number Pattern
+n = 4
+Output:
+1
+2 2
+3 3 3
+4 4 4 4
+6. Alphabet Triangle
+n = 4
+Output: 
+A
+A B
+A B C
+A B C D
+
+7. Floyd Triangle
+n = 4
+Output:
+1
+2 3
+4 5 6
+7 8 9 10
+
+8. Hollow Square
+n = 4
+Output:
+* * * *
+*     *
+*     *
+* * * *
+'''
---
+++ IV th-sem/Python/Batch-1C/practice/M02_Logic_Building_and_Patterns/S02/PS06_Intermediante_patterns.py
@@ -0,0 +1,29 @@
+'''Intermediante patterns
+1. Pyramid
+n = 4
+Output:
+   *
+  * *
+ * * *
+* * * *
+
+2. Inverted Pyramid
+n=4
+Output:
+* * * *
+ * * *
+  * *
+   *
+
+3. Diamond
+n=4
+Output:
+   *
+  * *
+ * * *
+* * * *
+ * * *
+  * *
+   *
+
+'''
