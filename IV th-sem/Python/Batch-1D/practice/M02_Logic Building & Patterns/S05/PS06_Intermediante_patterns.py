18f3d395ac9a66e5b686834e5740fdbe4dc5ee21
preeti <preeti@dhyanahitha.com>
2026-02-20 07:40:59 +0530

    18-2-2026

---
+++ IV th-sem/Python/Batch-1D/practice/M02_Logic Building & Patterns/S05/PS06_Intermediante_patterns.py
@@ -0,0 +1,87 @@
+'''Intermediante patterns
+1. Pyramid
+n = 4
+Output:
+   *
+  * *
+ * * *
+* * * *
+
+2. Inverted Pyramid
+n=4
+Output:
+* * * *
+ * * *
+  * *
+   *
+
+3. Diamond
+n=4
+Output:
+   *
+  * *
+ * * *
+* * * *
+ * * *
+  * *
+   *
+4. Number Pyramid
+n=4
+Output:
+   1
+  1 2
+ 1 2 3
+1 2 3 4
+
+5. Palindrome Pattern
+n=4
+Output:
+1
+212
+32123
+4321234
+
+6. Reverse Number Triangle
+n=4
+Output:
+1 2 3 4
+1 2 3
+1 2
+1
+
+7. Binary Triangle
+n=4
+Output:
+1
+0 1
+1 0 1
+0 1 0 1
+
+8. Cross Pattern
+n=5
+Output:
+*       *
+  *   *
+    *
+  *   *
+*       *
+
+9. Hollow Diamond
+n=4
+Output:
+   *
+  * *
+ *   *
+*     *
+ *   *
+  * *
+   *
+
+10. Hollow Pyramid
+n=4
+Output:
+   *
+  * *
+ *   *
+*******
+'''
--- IV th-sem/Python/Batch-1D/practice/M02_Logic Building & Patterns/S05/PS07_Advanced_patterns.py
+++ IV th-sem/Python/Batch-1D/practice/M02_Logic Building & Patterns/S05/PS07_Advanced_patterns.py
@@ -7,15 +7,7 @@ Output:
 1 2 1
 1 3 3 1
 1 4 6 4 1
-n = int(input())
-for i in range(n):
-    num = 1
-    for j in range(i+1):
-        print(num,end=" ")
-        num = num * (i-j) // (j+1)
-    print()
-'''
-'''
+
 2. Butterfly Pattern
 n=4
 Output:
@@ -27,14 +19,7 @@ Output:
 ***  ***
 **    **
 *      *
-'''
-n = int(input())
-for i in range(1,n+1):
-    print("*"*i + " "*(2*(n-i)) + "*"*i)
-for i in range(n,0,-1):
-    print("*"*i + " "*(2*(n-i)) + "*"*i)
 
-'''
 3. Hourglass Pattern
 n=4
 Output:
