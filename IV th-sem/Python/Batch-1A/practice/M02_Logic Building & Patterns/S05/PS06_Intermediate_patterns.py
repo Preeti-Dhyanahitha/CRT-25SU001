28d8aa86402e58d64cc1a8e89215affcea009a9b
preeti <preeti@dhyanahitha.com>
2026-02-23 12:22:01 +0530

    23-2-2026

--- IV th-sem/Python/Batch-1A/practice/M02_Logic Building & Patterns/S05/PS06_Intermediate_patterns.py
+++ IV th-sem/Python/Batch-1A/practice/M02_Logic Building & Patterns/S05/PS06_Intermediate_patterns.py
@@ -6,10 +6,6 @@ Output:
   * *
  * * *
 * * * *
-n = int(input())
-for i in range(1,n+1):
-    print(" "*(n-i)+"* "*i)
-    
 
 2. Inverted Pyramid
 n=4
@@ -89,85 +85,65 @@ Output:
  *   *
 *******
 
-'''
-'''
 li = [1,2,3,4,5]
-#output : [2,4,6,8,10]
+#output : [1,4,9,16,25]
 res = []
 for i in li:
-    res.append(i * 2)
+    res.append(i ** 2)
 print(res)
 
-print([i*2 for i in li])
+ans = [i**2 for i in li]
+print(ans)
 
 li = [1,2,3,4,5]
-#output : [2,4]
 res = []
 for i in li:
     if i % 2 == 0:
         res.append(i)
 print(res)
 
-print([i for i in li if i % 2 == 0])
+ans = [i for i in li if i % 2 == 0]
+print(ans)
 
-#['a','b','c'] ==> "abc"
-li1 = ['a','b','c']
+print(" * "*5)
+li = ['a','b','c'] #'a b c'
 res = ""
-for ch in li1:
-    res += ch
-print(res) 
-print("".join(li1))
+for ch in li:
+    res = res + ch + " "
+print(res)
 
-2. Inverted Pyramid
-n=4
-Output:
-* * * *
- * * *
-  * *
-   *
-3. Diamond
-n=4
+print("@".join(li))
+
+1. Pyramid
+n = 4
 Output:
    *
   * *
  * * *
 * * * *
- * * *
-  * *
-   *
 
 n = int(input())
 for i in range(1,n+1):
     print(" "*(n-i)+"* "*i)
-for i in range(n-1,0,-1):
-    print(" "*(n-i) + "* "*i)
+print("----------------")
+for i in range(n,0,-1):
+    print(" "*(n-i)+"* "*i)
 
-4. Number Pyramid
+n = int(input())
+for i in range(1,n+1):
+    print(" "*(n-i)+"* "*i)
+
+for i in range(n-1,0,-1):
+    print(" "*(n-i)+"* "*i)
+
+Number Pyramid
 n=4
 Output:
    1
   1 2
  1 2 3
 1 2 3 4
-
+'''
 n = int(input())
 for i in range(1,n+1):
     print(" "*(n-i)+" ".join([str(j) for j in range(1,i+1)]))
-
-for i in range(1,n+1):
-    print(" "*(n-i)+" ".join([str(i) for j in range(1,i+1)]))
-
-n = 4
-output:
-A
-B C
-D E F
-G H I J
-'''
-n = int(input())
-val = 65
-for i in range(n):
-    for j in range(i+1):
-        print(chr(val),end=" ")
-        val += 1
-    print()
