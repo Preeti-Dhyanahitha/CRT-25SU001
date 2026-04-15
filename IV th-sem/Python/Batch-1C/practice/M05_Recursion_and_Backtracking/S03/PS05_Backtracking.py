3f4664267ed77060f30d70ee366dd51d276c32dd
preeti <preeti@dhyanahitha.com>
2026-04-15 16:44:18 +0530

    15-4-2026

--- IV th-sem/Python/Batch-1C/practice/M05_Recursion_and_Backtracking/S03/PS05_Backtracking.py
+++ IV th-sem/Python/Batch-1C/practice/M05_Recursion_and_Backtracking/S03/PS05_Backtracking.py
@@ -1,1 +1,15 @@
 #Subsets, permutations
+
+def subsets(nums):
+    res = []
+    n = len(nums)
+    stop_value = 2 ** n
+    for i in range(stop_value):
+        subset = []
+        for j in range(n):
+            if i & (1 << j):
+                subset.append(nums[j])
+        res.append(subset)
+    return res
+        
+print(subsets([1,2,3]))
